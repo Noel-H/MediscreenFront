cd35bc896daf047f573a70bb05e9a6daff35338b
Noel-H <noel.huberto@gmail.com>
2022-09-04 13:17:58 +0200

    Initial commit

---
+++ src/test/java/com/noelh/mediscreenfront/MediscreenFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noelh.mediscreenfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediscreenFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
